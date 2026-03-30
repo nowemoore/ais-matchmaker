7c6578a2f42ce2091910406f10b740de911d1a0c
nowemoore <nowe.moore@gmail.com>
2026-03-30 11:57:50 +0100

    fixed typo

--- src/app/quiz/QuizClient.tsx
+++ src/app/quiz/QuizClient.tsx
@@ -83,7 +83,7 @@ export default function QuizClient({ userId }: Props) {
         href="/"
         className="absolute top-6 left-6 inline-flex items-center gap-2 text-sm text-white/50 hover:text-white/90 transition-colors"
       >
-        <FontAwesomeIcon icon={faChevronLeft} className="w-3 h-3" />
+        <FontAwesomeIcon icon={faCircleChevronLeft} className="w-3 h-3" />
         Home
       </Link>
 
